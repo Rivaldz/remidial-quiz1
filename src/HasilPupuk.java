878d0cdcc4d57fa68d690dd9f1255e7118708b9a
rivaldz <rivaldz@Rivaldz>
2018-11-04 13:59:18 +0700

    Mengisi kontructor

---
+++ src/HasilPupuk.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rivaldz
+ */
+public class HasilPupuk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Pupuk ppk = new Pupuk(5, "Urea", "chiherang", 2);
+        
+        
+    }
+    
+}
