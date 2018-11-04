a67b27f03137d6b901f4aeb723140fdac8f57bdb
rivaldz <rivaldz@Rivaldz>
2018-11-04 16:19:03 +0700

    Mengubah menjadi inputan

--- src/HasilPupuk.java
+++ src/HasilPupuk.java
@@ -8,6 +8,8 @@
  *
  * @author rivaldz
  */
+import java.util.Scanner;
+
 public class HasilPupuk {
 
     /**
@@ -15,13 +17,19 @@ public class HasilPupuk {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
         
-        Pupuk pupuk = new Pupuk(3, "phonska", "Ciherang", 2);
-        pupuk.hitungPupuk();
-        
-        
-        
-        
+        System.out.println("Masukan berat = (gram)");
+        int berat = input.nextInt();
+        System.out.println("Masukan jenis pupuk = ");
+        String jp = input.next();
+        System.out.println("Masukan jenis Padi = ");
+        String jP = input.next();
+        System.out.println("Masukan umur padi  = ");
+        int umur = input.nextInt();
+        Pupuk pupuk = new Pupuk(berat,jp, jP, umur);
+        pupuk.tampilan();
+           
     }
     
 }
