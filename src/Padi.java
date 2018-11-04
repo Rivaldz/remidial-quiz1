bd17ee7fdcafeff8ee973b111a3860f98fc53a0b
rivaldz <rivaldz@Rivaldz>
2018-11-04 13:52:17 +0700

    Membuat kontructor dan juga setter getter

--- src/Padi.java
+++ src/Padi.java
@@ -11,6 +11,26 @@
 public class Padi {
  private String jenisPadi;   
  private int umurPadi;
- 
- 
+
+    public Padi(String jenisPadi, int umurPadi) {
+        this.jenisPadi = jenisPadi;
+        this.umurPadi = umurPadi;
+    }
+
+    public String getJenisPadi() {
+        return jenisPadi;
+    }
+
+    public void setJenisPadi(String jenisPadi) {
+        this.jenisPadi = jenisPadi;
+    }
+
+    public int getUmurPadi() {
+        return umurPadi;
+    }
+
+    public void setUmurPadi(int umurPadi) {
+        this.umurPadi = umurPadi;
+    }
+  
 }
