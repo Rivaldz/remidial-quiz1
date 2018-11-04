c509e8c83b555c03b1d75358acce5bb6c78228ac
rivaldz <rivaldz@Rivaldz>
2018-11-04 13:55:48 +0700

    pembuatan contraktor dan juga  setter getter di class Pupuk

---
+++ src/Pupuk.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rivaldz
+ */
+public class Pupuk extends Padi{
+    
+private int berat;
+private  String jenisPupuk;
+
+    public Pupuk(int berat, String jenisPupuk, String jenisPadi, int umurPadi) {
+        super(jenisPadi, umurPadi);
+        this.berat = berat;
+        this.jenisPupuk = jenisPupuk;
+    }
+
+    public int getBerat() {
+        return berat;
+    }
+
+    public void setBerat(int berat) {
+        this.berat = berat;
+    }
+
+    public String getJenisPupuk() {
+        return jenisPupuk;
+    }
+
+    public void setJenisPupuk(String jenisPupuk) {
+        this.jenisPupuk = jenisPupuk;
+    }
+
+    
+
+
+
+}
+
+ 
